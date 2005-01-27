a290e302e8d2951e197f9ba0dbf3bcb50472cf88
Ann W. Harrison <awharrison@users.sourceforge.net>
2005-01-27 19:04:24 +0000

    don't copy the solution file

--- boot_copy.bat
+++ boot_copy.bat
@@ -6,7 +6,3 @@ type dsql\array.boot >dsql\array.cpp
 type gpre\gpre_meta.boot >gpre\gpre_meta.cpp
 type include\gen\autoconfig_msvc.h >include\gen\autoconfig.h
 cd ..
-mkdir Vulcan
-copy Vulcan.sln.template Vulcan\Vulcan.sln
-
-
