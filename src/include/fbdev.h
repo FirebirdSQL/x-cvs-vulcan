bc56e276e7fb7ac3bed5f57f15c9a9183989995c
Ann W. Harrison <awharrison@users.sourceforge.net>
2006-01-13 21:55:54 +0000

    Fix the license per agreement with creators

--- src/include/fbdev.h
+++ src/include/fbdev.h
@@ -2,25 +2,17 @@
 #define INCLUDE_FBDev
 
 /*
- *  The contents of this file are subject to the Mozilla Public
- *  License Version 1.1 (the "License"); you may not use this file
- *  except in compliance with the License. You may obtain a copy of
- *  the License at http://www.mozilla.org/MPL/
- *  Alternatively, the contents of this file may be used under the
- *  terms of the GNU General Public License Version 2 or later (the
- *  "GPL"), in which case the provisions of the GPL are applicable
- *  instead of those above. You may obtain a copy of the Licence at
- *  http://www.gnu.org/copyleft/gpl.html
+ *  
+ *     The contents of this file are subject to the Initial 
+ *     Developer's Public License Version 1.0 (the "License"); 
+ *     you may not use this file except in compliance with the 
+ *     License. You may obtain a copy of the License at 
+ *     http://www.ibphoenix.com/idpl.html. 
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  Relevant for more details.
- *
- *  This file was created by members of the firebird development team.
- *  All individual contributions remain the Copyright (C) of those
- *  individuals.  Contributors to this file are either listed here or
- *  can be obtained from a CVS history command.
+ *     Software distributed under the License is distributed on 
+ *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
+ *     express or implied.  See the License for the specific 
+ *     language governing rights and limitations under the License.
  *
  *  All rights reserved.
  *
