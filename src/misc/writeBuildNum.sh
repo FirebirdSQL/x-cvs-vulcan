7455c5f43a126c018c0c6054251346dbc9ba11a7
Jim Starkey <jimstarkey@users.sourceforge.net>
2006-03-22 15:16:15 +0000

    increment build number

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -9,7 +9,7 @@ BuildType=V
 MajorVer=2
 MinorVer=0
 RevNo=0
-BuildNum=4545
+BuildNum=4546
 #BuildSuffix="Firebird 1.5 Release Candidate 7"
 BuildSuffix="Vulcan 1.0 Development"
 FIREBIRD_PACKAGE_VERSION=RC7
@@ -83,7 +83,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=4545
+BuildNum=4546
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
