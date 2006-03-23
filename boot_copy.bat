1294aa391def25b38ef45bc02aaecbe80dd2304f
Paul Reeves <paul_reeves@users.sourceforge.net>
2006-03-23 11:31:17 +0000

    Remove files made redundant by new build system. They are now in their appropriate builds/$platform directories or in the doc dir.

--- boot_copy.bat
+++
@@ -1,8 +0,0 @@
-cd src
-type burp\backup.boot >burp\backup.cpp 
-type burp\restore.boot >burp\restore.cpp
-type dsql\blob.boot >dsql\blob.cpp
-type dsql\array.boot >dsql\array.cpp
-type gpre\gpre_meta.boot >gpre\gpre_meta.cpp
-type include\gen\autoconfig_msvc.h >include\gen\autoconfig.h
-cd ..
