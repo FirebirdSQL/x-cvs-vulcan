e5fa0439a0a2f74c3651b7d0d232f431216cf12a
Adriano dos Santos Fernandes <asfernandes@users.sourceforge.net>
2006-09-06 02:29:31 +0000

    Misc

--- src/isql/isql.h
+++ src/isql/isql.h
@@ -19,7 +19,6 @@
  *
  * All Rights Reserved.
  * Contributor(s): ______________________________________.
- * $Id$
  * Revision 1.2  2000/11/18 16:49:24  fsg
  * Increased PRINT_BUFFER_LENGTH to 2048 to show larger plans
  * Fixed Bug #122563 in extract.e get_procedure_args
